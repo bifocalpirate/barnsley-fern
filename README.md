063c4b476b4e59bca6422235f4495b4fbd7c0506
marmite@dollar <mailbox@a20.co.za>
2017-03-22 07:25:50 +0200

    temp checkin while adding parameter change for IFS fractal exploration (currently buggy)

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Barnsley Fern
+* You need an ES2016-compatible browser for this to work. Chrome should work. 
 
 ![alt tag](https://raw.githubusercontent.com/skrillex581/barnsley-fern/master/fern-img.gif)
 
-A barnsley fern using p5.js.
+This application draws an IFS fractal.  By default it draws a barnsley ferns. Play around with the ruleset, or add your own, to explore additional fractals.
 
-https://en.wikipedia.org/wiki/Barnsley_fern
+* https://en.wikipedia.org/wiki/Barnsley_fern
+* https://en.wikipedia.org/wiki/Iterated_function_system
--- js/app.js
+++ js/app.js
@@ -1,90 +1,189 @@
 /* configs */
-var CANVAS_WIDTH = 600;
-var CANVAS_HEIGHT = 600;
+var CANVAS_WIDTH = 300;
+var CANVAS_HEIGHT = 300;
 var FRAME_RATE = 60;
+
+/* rt settings */
+
+var panels = [];
+var controlPanel = null;
+var panelCount = 0; //until the user clicks apply settings do we use the new config collection
 /* globals */
 var x = Math.random(), y = Math.random();
+var rules = [];
+var currentRuleSetName = "";
+var ruleSets = {
+    "Thelypteridaceae":[
+                {
+    
+                    a:0,
+                    b:0,
+                    c:0,
+                    d:0.25,
+                    x:0,
+                    y:-0.4,
+                    p:0.02 //probability of selection        
+                },
+                {
+                    a:0.95,
+                    b:0.005,
+                    c:-0.005,
+                    d:0.93,
+                    x:-.002,
+                    y:0.5,
+                    p:0.84
+                },    
+                {
+                    a:0.035,
+                    b:-0.2,
+                    c:0.16,
+                    d:0.04,
+                    x:-0.09,
+                    y:0.02,
+                    p:0.07
+                },
+                {
+                    a:-0.04,
+                    b:0.2,
+                    c:0.16,
+                    d:0.04,
+                    x:0.083,
+                    y:0.12,
+                    p:0.07
+                }                                
+    ],
+    "Barnsleyfern":[  
+                {
+                    a:0.85,
+                    b:0.04,
+                    c:-0.04,
+                    d:0.85,
+                    x:0,
+                    y:1.60,
+                    p:0.85
+                },
+                {
+                    a:-0.15,
+                    b:0.28,
+                    c:0.26,
+                    d:0.24,
+                    x:0,
+                    y:0.44,
+                    p:0.07
+                },
+                {
+                    a:0.2,
+                    b:-0.26,
+                    c:0.23,
+                    d:0.22,
+                    x:0,
+                    y:1.60,
+                    p:0.07
+                },    
+                {
+                    a:0,
+                    b:0,
+                    c:0,
+                    d:0.16,
+                    x:0,
+                    y:0,
+                    p:0.01 //probability of selection        
+                }
+            ]
 
-var rules = [  
-    {
-        a:0.85,
-        b:0.04,
-        c:-0.04,
-        d:0.85,
-        x:0,
-        y:1.60,
-        p:0.85,
-        color: '#5499C7'
-    },
-    {
-        a:-0.15,
-        b:0.28,
-        c:0.26,
-        d:0.24,
-        x:0,
-        y:0.44,
-        p:0.07,
-        color:'#34495E'
-    },
-    {
-        a:0.2,
-        b:-0.26,
-        c:0.23,
-        d:0.22,
-        x:0,
-        y:1.60,
-        p:0.07,
-        color:'#C70039'
-    },    
-    {
+    
+}
+
+function addPanel(config)
+{            
+            p = QuickSettings.create(CANVAS_WIDTH + panels.length*260,50,"Rules " + (panels.length+1))
+            .setDraggable(true)
+            .setCollapsible(true)                                                   
+            Object.keys(config).forEach(function(k) {
+                p.addRange(k,0,1,config[k],0.01);                             
+            });
+            panels.push(p);
+            console.log(p.getValuesAsJSON());
+}
+/* configs */
+function setup() {
+    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);    
+    frameRate(FRAME_RATE);     
+    controlPanel = QuickSettings.create(50,CANVAS_HEIGHT+100,"Controls");
+    controlPanel.addDropDown("Rule Sets", 
+    Object.keys(ruleSets)
+    );
+    currentRuleSetName = controlPanel.getValue("Rule Sets");
+    controlPanel.addButton("Add Rule", ()=>{addRule()});
+    controlPanel.addButton("Refresh Settings", ()=>{resetRules()});
+    controlPanel.addButton("Apply Settings", () => { applyRules()});
+    strokeWeight(0.5);
+    stroke('#34495E');        
+    resetRules(); //clear panels, load default rules       
+}
+function resetRules()
+{
+    panels.forEach((a) => {
+            a.destroy();
+    })
+    panels = [];
+    addPanel(ruleSets.Barnsleyfern[0]); //make sure this is a deep copy   
+    addPanel(ruleSets.Barnsleyfern[1]);    
+    addPanel(ruleSets.Barnsleyfern[2]);    
+    addPanel(ruleSets.Barnsleyfern[3]); 
+    applyRules();  
+}
+
+function applyRules()
+{        
+    console.log('Apply rules');        
+    clear();    //clear the canvas
+    //cycle all the way up (read all the rules in)
+    panelCount = panels.length;
+}
+
+function keyPressed() {
+    clear();
+}
+function addRule()
+{
+    var d = {
         a:0,
         b:0,
         c:0,
         d:0.16,
         x:0,
         y:0,
-        p:0.01, //probability of selection
-        color:'#51B422'
+        p:0.01 //probability of selection        
     }
-];
-
-/* configs */
-function setup() {
-    createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);    
-    //frameRate(FRAME_RATE); 
-    
-    strokeWeight(0.5);
-    stroke('#34495E');    
-    //rect(0, 0, CANVAS_WIDTH-1, 0);
+    addPanel("Rule " + (panels.length+1),d);
 }
 
-function keyPressed() {
-    pause = !pause;
-}
 
-function draw() {    
+function draw() {      
     translate(CANVAS_WIDTH/2,CANVAS_HEIGHT); //call to translate is reset between draw()
-    for(var j=0; j < 100; j++)
+    for(var j=0; j < 10; j++)
     {
             var rule = getRule();
             var x1 = x * rule.a + y * rule.b + rule.x;
             var y1 = x * rule.c + y * rule.d + rule.y;
             x = x1;
             y = y1;    
-            drawPoint(x,y,rule.color);
+            drawPoint(x,y,'green');
     }    
  }
  function drawPoint(x,y,color)
  {         
-    stroke(color);
-    point(x*50,-y*50);
+     if (color)
+        stroke(color);
+    point(x*30,-y*30);
  }
  function getRule()
- {
+ {     
     var rand = Math.random();
-    for(var i=0; i < rules.length; i++)
+    for(var i=0; i < panelCount; i++)
      {
-         var rule = rules[i];
+         var rule = panels[i].getValuesAsJSON();
          if (rand < rule.p) return rule;
          rand -= rule.p;
      }          
